bd2ca2a93dbcbd7d1b64f9beaa912b1aacf7812b
flaviorv <frezendevaroto@gmail.com>
2025-03-19 16:35:41 -0300

    Adjacents_of method that returns all connections of a neighborhood.

--- neighborhoods.py
+++ neighborhoods.py
@@ -9,6 +9,10 @@ class WeightedGraph:
             self.vertices[neighborhood] = {}
 
     def connect(self, neighborhood1, neighborhood2, distance):
+        if neighborhood1 not in self.vertices:
+            self.vertices[neighborhood1] = {}
+        if neighborhood2 not in self.vertices:
+            self.vertices[neighborhood2] = {}
         self.vertices[neighborhood1][neighborhood2] = distance
         self.vertices[neighborhood2][neighborhood1] = distance 
 
@@ -35,12 +39,13 @@ class WeightedGraph:
         path.append(origin)
         path.reverse()
         return path, distances[destination]
+    
+    def adjacents_of(self, neighborhood):
+        return list(self.vertices[neighborhood].keys())
 
 if __name__ == "__main__":
     graph = WeightedGraph()
     neighborhoods = ["São José", "Santa Rita", "Três Marias", "São Sebastião", "Dona Lalá", "Alto dos Ypês"]
-    for neighborhood in neighborhoods:
-        graph.add(neighborhood)
     roads = [
         ("São José", "Alto dos Ypês", 6), ("São José", "Santa Rita", 4),
         ("Dona Lalá", "São José", 9), ("São Sebastião", "São José", 11),
@@ -55,3 +60,14 @@ if __name__ == "__main__":
         destination = s[1]
         path, distance = graph.dijkstra(origin, destination)
         print(f"Best path from {origin} to {destination}: ({' => '.join(path)}) Distance: {distance} km")
+    for neighborhood in neighborhoods:
+        print(f"Neighborhood: {neighborhood} Road to: {list(graph.vertices[neighborhood].keys())}")
+    graph2 = WeightedGraph()
+    roads = [("Centro", "Bairro A", 2), ("Centro", "Bairro B", 3), ("Bairro A", "Bairro C", 1),
+        ("Bairro B", "Bairro C", 3), ("Bairro C", "Bairro D", 4)
+    ]
+    for neighborhood1, neighborhood2, distance in roads:
+        graph2.connect(neighborhood1, neighborhood2, distance)
+    for neighborhood in graph2.vertices:
+        print(f"Neighborhood: {neighborhood} Road to: {graph2.adjacents_of(neighborhood)}")
+
